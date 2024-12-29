fe3f85ed7f984977cee89e2813adaf21cc7dd9e6
SYEONIH <113624562+SYEONIH@users.noreply.github.com>
2024-12-29 19:04:28 +0900

    [level 0] Title: 카운트 업, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.js
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.js
@@ -1,6 +1,6 @@
 function solution(start_num, end_num) {
     var answer = [];
-    for (;start_num<=end_num ; start_num++ ){
+    for (start_num;start_num<=end_num ; start_num++ ){
     answer.push(start_num);
     }
     return answer;
